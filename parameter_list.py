c77c32e047146523615529230b322f540f8f18cf
Pandabearingstreet <38286947+Pandabearingstreet@users.noreply.github.com>
2019-04-09 19:01:33 +0200

    Update parameter_list.py

--- parameter_list.py
+++ parameter_list.py
@@ -50,11 +50,11 @@ BG_COLOR = (123,123,123)
 FONTSIZE = 60
 # Set number of trials in the experiment
 # How should you name the variable?
-# TODO
+NUMTRIAL = 100
 
 # Set number of nogo trials in the experiment
 # How should you name the variable?
-# TODO
+PCT_NOGO = 20
 
 # Set the time interval in seconds of the delay
 # from end of trial n and beginning of trial n+1
@@ -70,4 +70,5 @@ FPS = 60
 # Create a directory called 'Data' in your working-directory
 # where the experiment data for each subject is saved
 # before creating it check if the directory 'Data' already exists
-# TODO
+if not os.path.exists('Data'):
+    os.mkdir('Data')
